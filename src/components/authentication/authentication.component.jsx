088c109666126770c71d12327f740e097370d24d
Nikhil Patel <npatel385@usf.edu>
2022-06-17 16:23:34 -0400

    "authentication page component and sign-in form"

--- src/components/authentication/authentication.component.jsx
+++ src/components/authentication/authentication.component.jsx
@@ -1,22 +1,13 @@
-import {  signInWithGooglePopup, createUserDocumentFromAuth } from  '../../Utils/firebase/firebase.utils'; 
-
 import SignUpForm from '../sign-up-form/sign-up-form.component';
-
-
+import SignInForm from '../sign-in-form/sign-in-form.component';
+import './authentication.styles.scss'
 const Authentication = () =>{
 
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup(); 
-        const userDocRef = await createUserDocumentFromAuth(user); 
-    }
 
 
     return(
-        <div>
-            <h1>Sign In</h1>
-            <button onClick={logGoogleUser}>
-                sign in with Google Popup
-            </button>
+        <div className="authentication-container">
+            <SignInForm />
             <SignUpForm />
             
         </div>
